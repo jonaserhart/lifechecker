af841a0d4c64651639a4b80ff75ca061ff275930
Jonas Erhart <jonas.a.erhart@gmail.com>
2021-01-18 22:19:10 +0100

    /checks

--- config.js
+++ config.js
@@ -6,14 +6,16 @@ environments.staging = {
 	'httpPort': 3000,
 	'httpsPort': 3001,
 	'name': 'staging',
-	'hashingSecret' : 'thisIsASecret'
+	'hashingSecret' : 'thisIsASecret',
+	'maxChecks' : 5
 };
 
 environments.production = {
 	'httpPort': 5000,
 	'httpsPort': 5001,
 	'name': 'production',
-	'hashingSecret' : 'thisIsALongerSecret'
+	'hashingSecret' : 'thisIsALongerSecret',
+	'maxChecks' : 5
 };
 
 var env = typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : '';
--- index.js
+++ index.js
@@ -37,7 +37,8 @@ httpsServer.listen(config.httpsPort, () => {
 var router = {
 	'ping' : handlers.ping,
 	'users' : handlers.users,
-	'tokens' : handlers.tokens
+	'tokens' : handlers.tokens,
+	'checks' : handlers.checks
 };
 
 var main = function(req, res) {
